ef9e95a24352da45c6b964a3c4fbb3960ae4b37c
Kashyap Ramakur <69868711+kramakur@users.noreply.github.com>
2023-09-11 14:58:40 -0400

    moving vpc dns modules to NEDSS-Infrastructure (#4)

---
+++ terraform/aws/app-infrastructure/vpc/outputs.tf
@@ -0,0 +1,31 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_route_table_id" {
+  value = module.vpc.private_route_table_ids[0] 
+}
+
+output "public_route_table_id" {
+  value = module.vpc.public_route_table_ids[0] 
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
