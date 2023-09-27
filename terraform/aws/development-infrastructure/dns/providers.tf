18bdf56ff64e947a9ebc3c6c3137e3fc85f07ea0
szamfir <78826267+szamfir@users.noreply.github.com>
2023-09-27 09:17:28 -0400

    first commit of release 7.0.0

--- terraform/aws/development-infrastructure/dns/providers.tf
+++
@@ -1,8 +0,0 @@
-# Assume role in hosted-zone account
-provider "aws" {
-  alias = "hosted-zone-account"
-  assume_role {
-    role_arn     = var.hosted-zone-iam-arn
-    session_name = "deploy-base-infrastructure"
-  }
-}
--- terraform/aws/development-infrastructure/nbs-legacy/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "nbs_db_address" {
-  value = module.db.db_instance_address
-}
-
-output "nbs_app_alb" {
-  value = module.alb.lb_dns_name
-}
