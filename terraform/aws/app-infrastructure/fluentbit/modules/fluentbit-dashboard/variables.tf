7327efca8dfb2420573e8c5b4a9fd7b56c7fc57a
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2023-11-06 08:46:00 -0500

    added dataingestion and modernization-api to fluentbit dashboard

--- terraform/aws/app-infrastructure/fluentbit/modules/fluentbit-dashboard/variables.tf
+++ terraform/aws/app-infrastructure/fluentbit/modules/fluentbit-dashboard/variables.tf
@@ -2,7 +2,7 @@ variable "log_group_name" { default = "fluent-bit-cloudwatch"}
 
 variable "app_name" {
   type    = list(string)
-  default = ["argocd", "elasticsearch", "fluentbit", "ingress-nginx", "istio", "nifi", "patient-search", "prometheus" ]
+  default = ["argocd", "elasticsearch", "fluentbit", "ingress-nginx", "istio", "nifi", "patient-search", "prometheus", "modernization-api", "dataingestion"]
 }
 
 
