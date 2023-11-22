ee0bf491c68e9c45fbdf378b4c7588d4947ae976
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2023-11-22 11:50:18 -0500

    fixed msk second time

--- terraform/aws/app-infrastructure/msk/outputs.tf
+++ terraform/aws/app-infrastructure/msk/outputs.tf
@@ -1,9 +1,9 @@
 output "bootstrap_brokers" {
   description = "The bootstrap brokers for the MSK cluster"
-  value       = aws_msk_cluster.this.bootstrap_brokers
+  value       = aws_msk_cluster.this[0].bootstrap_brokers
 }
 
 output "zookeeper_connect_string" {
   description = "The Zookeeper connect string for the MSK cluster"
-  value       = aws_msk_cluster.this.zookeeper_connect_string
+  value       = aws_msk_cluster.this[0].zookeeper_connect_string
 }
