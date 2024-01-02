caf379d1244f269e52071975484b31c4908bf4bb
Chuck Moss <cmoss@enquizit.com>
2024-01-02 12:50:25 -0500

    formatting

--- terraform/aws/samples/NBS7_standard/msk.tf
+++ terraform/aws/samples/NBS7_standard/msk.tf
@@ -1,16 +1,12 @@
 # Serial: 2024010201
 
 module "msk" {
-  #source              = "git::https://github.com/CDCgov/NEDSS-Infrastructure.git//terraform/aws/app-infrastructure/msk?ref=v1.1.0-DEV"
-  #source              = "git::https://github.com/CDCgov/NEDSS-Infrastructure.git//terraform/aws/app-infrastructure/msk?ref=v1.1.4-DEV"
   source              = "../app-infrastructure/msk"
   msk_subnet_ids      = module.modernization-vpc.private_subnets
   vpc_id              = module.modernization-vpc.vpc_id
   msk_ebs_volume_size = var.msk_ebs_volume_size
-  #modern-cidr         = var.modern-cidr
-  #vpn-cidr            = var.shared_vpc_cidr_block
   # now combining cidr blocks to make vpn cidr optional
-  cidr_blocks         = [var.modern-cidr] #can add this to list if needed: var.shared_vpc_cidr_block
+  cidr_blocks = [var.modern-cidr] # can add this to list if needed: var.shared_vpc_cidr_block
   #cidr_blocks     = [var.modern-cidr, var.shared_vpc_cidr_block]
   resource_prefix = var.resource_prefix
 }
