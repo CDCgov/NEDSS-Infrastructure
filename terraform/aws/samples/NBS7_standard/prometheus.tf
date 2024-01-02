8458910f47bcaad3904e5ece9b9da47e28fc079c
Chuck Moss <cmoss@enquizit.com>
2024-01-02 14:58:07 -0500

    removed cross account role

--- terraform/aws/samples/NBS7_standard/prometheus.tf
+++ terraform/aws/samples/NBS7_standard/prometheus.tf
@@ -8,6 +8,6 @@ module "prometheus" {
   eks_cluster_endpoint               = module.eks_nbs.eks_cluster_endpoint
   cluster_certificate_authority_data = module.eks_nbs.cluster_certificate_authority_data
   eks_cluster_name                   = module.eks_nbs.eks_cluster_name
-  eks_aws_role_arn                   = "arn:aws:iam::${var.target_account_id}:role/cdc-terraform-user-cross-account-role"
+  eks_aws_role_arn                   = "arn:aws:iam::${var.target_account_id}:role/${var.aws_admin_role_name}"
   namespace_name                     = module.eks_nbs.precreated_observability_namespace_name
 }
