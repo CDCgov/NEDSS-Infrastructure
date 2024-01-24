dcbb5eb356144e934a4b8cf327e218a826499f40
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2024-01-24 16:06:02 -0500

    commit

--- terraform/aws/app-infrastructure/eks-nbs/providers.tf
+++ terraform/aws/app-infrastructure/eks-nbs/providers.tf
@@ -11,3 +11,15 @@
 #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 #   token                  = data.aws_eks_cluster_auth.cluster.token
 # }
+
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}s
