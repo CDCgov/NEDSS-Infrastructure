5b7ddaa71ab63da79eeeec6d350c93019c97791c
Serban Zamfir <szamfir@enquizit.com>
2024-01-31 13:37:28 -0500

    fix resource ref

--- terraform/aws/app-infrastructure/eks-nbs/ecr.tf
+++ terraform/aws/app-infrastructure/eks-nbs/ecr.tf
@@ -5,7 +5,7 @@ resource "aws_ecr_pull_through_cache_rule" "ecr_public" {
     upstream_registry_url = "public.ecr.aws"
 }
 
-resource "aws_ecr_pull_through_cache_rule_" "quay" {
+resource "aws_ecr_pull_through_cache_rule" "quay" {
     count = var.use_ecr_pull_through_cache ? 1 : 0
     ecr_repository_prefix = "quay-public"
     upstream_registry_url = "quay.io"
