1becf5c6a5485c07f84ff43222a693404a0c6e2d
Serban Zamfir <szamfir@enquizit.com>
2024-01-31 13:32:30 -0500

    rename resource

--- terraform/aws/app-infrastructure/eks-nbs/ecr.tf
+++ terraform/aws/app-infrastructure/eks-nbs/ecr.tf
@@ -1,17 +1,17 @@
 # This creates a pull through cache for public AWS ECR, quay, and dockerhub
-resource "aws_ecr_pull_through_cache_rule" "example" {
+resource "aws_ecr_pull_through_cache_rule" "ecr_public" {
     count = var.use_ecr_pull_through_cache ? 1 : 0
     ecr_repository_prefix = "ecr-public"
     upstream_registry_url = "public.ecr.aws"
 }
 
-resource "aws_ecr_pull_through_cache_rule" "example" {
+resource "aws_ecr_pull_through_cache_rule_" "quay" {
     count = var.use_ecr_pull_through_cache ? 1 : 0
     ecr_repository_prefix = "quay-public"
     upstream_registry_url = "quay.io"
 }
 
-resource "aws_ecr_pull_through_cache_rule" "example" {
+resource "aws_ecr_pull_through_cache_rule" "dockerhub" {
     count = var.use_ecr_pull_through_cache ? 1 : 0
     ecr_repository_prefix = "dockerhub-public"
     upstream_registry_url = "registry-1.docker.io"
