873fa980bb646d421cfc8e76cc249ed56b34aa66
Serban Zamfir <szamfir@enquizit.com>
2024-02-05 14:06:51 -0500

    revert subnets to list

--- terraform/aws/samples/NBS7_2_subnet/data.tf
+++ terraform/aws/samples/NBS7_2_subnet/data.tf
@@ -22,5 +22,5 @@ locals {
   list_azs = [data.aws_subnet.subnet_c.availability_zone,data.aws_subnet.subnet_d.availability_zone]
   list_subnet_cidr = [data.aws_subnet.subnet_c.cidr_block,data.aws_subnet.subnet_d.cidr_block]
   list_subnet_ids = [data.aws_subnet.subnet_c.id,data.aws_subnet.subnet_d.id]
-  csv_list_db_subnet_ids = [data.aws_subnet.db_subnet_b.id,data.aws_subnet.db_subnet_c.id]
+  list_db_subnet_ids = [data.aws_subnet.db_subnet_b.id,data.aws_subnet.db_subnet_c.id]
 }
