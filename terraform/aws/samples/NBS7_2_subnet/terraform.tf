b9d582e6c3ad1b831cff5a48fa0264b278fb060b
Serban Zamfir <szamfir@enquizit.com>
2024-02-14 11:11:03 -0500

    update terraform.tf

--- terraform/aws/samples/NBS7_2_subnet/terraform.tf
+++ terraform/aws/samples/NBS7_2_subnet/terraform.tf
@@ -8,9 +8,9 @@ terraform {
   # Precreate s3 bucket named "cdc-nbs-sandbox-terraform"
   backend "s3" {
     encrypt = true
-    #change following 2 lines and comment this one
-    bucket  = "cdc-nbs-test-terraform"
-    key = "cdc-nbs-test/infrastructure-artifacts"
+    #change following 2 lines and uncomment them
+    # bucket  = "cdc-nbs-test-terraform"
+    # key = "cdc-nbs-test/infrastructure-artifacts"
     region = "us-east-1"
   }
 }
