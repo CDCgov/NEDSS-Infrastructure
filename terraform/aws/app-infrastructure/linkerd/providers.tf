d854aa08a2995e54366ffaec084791191061ba7a
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2024-02-21 09:56:05 -0500

    linkerd module

---
+++ terraform/aws/app-infrastructure/linkerd/providers.tf
@@ -0,0 +1,14 @@
+
+provider "helm" {
+  kubernetes {
+    host                   = var.eks_cluster_endpoint                             # module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data) # base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
---
+++ terraform/aws/app-infrastructure/linkerd/variables.tf
@@ -0,0 +1,19 @@
+
+variable "linkerd_repository" {
+    default = "https://helm.linkerd.io/stable"
+}
+variable "linkerd_chart" {
+    default = "linkerd-crds"
+}
+variable "linkerd_namespace_name" {
+    default = "linkerd"
+}
+variable "linkerd_controlplane_chart" {
+    default = "linkerd/linkerd-control-plane"
+}
+variable "linkerd_viz_chart" {
+    default = "linkerd-viz"
+}
+variable "linkerd_viz_namespace_name" {
+    default = "linkerd-viz"
+}
