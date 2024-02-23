d7b6f07b2abbf585681a6572407044f844938a16
Chuck Moss <cmoss@enquizit.com>
2024-02-23 15:04:29 -0500

    pinned to a version and formatted

--- terraform/aws/app-infrastructure/eks-nbs/irsa.tf
+++ terraform/aws/app-infrastructure/eks-nbs/irsa.tf
@@ -1,13 +1,14 @@
 module "efs_cni_irsa_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.34.0"
 
-  role_name = "${local.eks_name}-efs-cni" #defined in main.tf
-  create_role = true
+  role_name             = "${local.eks_name}-efs-cni" #defined in main.tf
+  create_role           = true
   attach_efs_csi_policy = true
 
   oidc_providers = {
     main = {
-      provider_arn               = module.eks.oidc_provider_arn
+      provider_arn = module.eks.oidc_provider_arn
       namespace_service_accounts = [
         "kube-system:aws-node",
         "kube-system:efs-csi-controller-sa"
@@ -17,10 +18,11 @@ module "efs_cni_irsa_role" {
 }
 
 module "cert_manager_cni_irsa_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.34.0"
 
-  role_name = "${local.eks_name}-cert-manager-cni" #defined in main.tf
-  create_role = true
+  role_name                  = "${local.eks_name}-cert-manager-cni" #defined in main.tf
+  create_role                = true
   attach_cert_manager_policy = true
 
   oidc_providers = {
