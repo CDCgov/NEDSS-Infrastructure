4cdd02ef45a3da5412e914e7d709e35011954d46
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2024-03-20 15:53:31 -0400

    changed the kafka module

--- terraform/azure/modules/2_nbs7/hdi-kafka/data.tf
+++ terraform/azure/modules/2_nbs7/hdi-kafka/data.tf
@@ -6,7 +6,7 @@ data "azurerm_resource_group" "rg" {
 # Get vNet Data
 data "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = var.vnet_rg
+  resource_group_name = var.resource_group_name
 }
 
 # Get Kafka Subnet Data
