482addf6ebcf35e11be539e87419ddbd1ca340d1
szamfir <78826267+szamfir@users.noreply.github.com>
2024-03-25 14:33:02 -0400

    update fluenbit for azure blob

--- terraform/azure/modules/3_applications/fluentbit/main.tf
+++ terraform/azure/modules/3_applications/fluentbit/main.tf
@@ -70,7 +70,7 @@ resource "helm_release" "fluentbit" {
             account_name          ${var.blob_account_name}
             shared_key            ${var.blob_shared_key}
             path                  kubernetes
-            container_name        fluentbit-logs
+            container_name        ${var.azure_container_name}
             auto_create_container on
             tls                   on
     EOF
