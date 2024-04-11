83af5a97a8a87cd5d2c2d5fa58af2231554be698
Chuck Moss <cmoss@enquizit.com>
2024-04-11 10:11:45 -0400

    minor cleanup/tweaks

--- scripts/install/aws_cloudshell_quickstart/08_cloudshell_get_helm_values_from_existing_environment.sh
+++ scripts/install/aws_cloudshell_quickstart/08_cloudshell_get_helm_values_from_existing_environment.sh
@@ -14,17 +14,6 @@ load_defaults() {
     fi
 }
 
-# Function to update or add variable values in the defaults file
-#update_defaults() {
-#    local var_name=$1
-#    local var_value=$2
-#    if grep -q "^$var_name=" "$DEFAULTS_FILE"; then
-#        sed -i "s/^$var_name=.*/$var_name=$var_value/" "$DEFAULTS_FILE"
-#    else
-#        echo "$var_name=$var_value" >> "$DEFAULTS_FILE"
-#    fi
-#}
-
 update_defaults() {
     local var_name=$1
     local var_value=$2
@@ -85,7 +74,7 @@ apply_substitutions_and_copy() {
     local new_file_path="${dest_dir}/${new_filename}"
 
     # Copy and apply substitutions
-    echo cp -ip "$src_file_path" "$new_file_path"
+    #echo cp -ip "$src_file_path" "$new_file_path"
     cp -ip "$src_file_path" "$new_file_path"
 
     # Apply substitutions
@@ -97,6 +86,9 @@ apply_substitutions_and_copy() {
     sed -i "s?EXAMPLE_LEGACY_CIDR_BLOCK?${LEGACY_CIDR_BLOCK}?" "$new_file_path"
     sed -i "s/EXAMPLE_BUCKET_NAME/${BUCKET_NAME}/" "$new_file_path"
     sed -i "s/EXAMPLE_ENVIRONMENT/${SITE_NAME}/g" "$new_file_path"
+    # TODO: FIXME: this needs to change in the template since
+    # EXAMPLE_DOMAIN is used elsewhere
+    sed -i "s/EXAMPLE_USER@EXAMPLE_DOMAIN/${CERT_MANAGER_EMAIL}/" "$new_file_path"
     sed -i "s/EXAMPLE_DOMAIN/${EXAMPLE_DOMAIN}/" "$new_file_path"
     sed -i "s/EXAMPLE_ACCOUNT_ID/${TMP_ACCOUNT_ID}/" "$new_file_path"
     sed -i "s/AWSReservedSSO_AWSAdministratorAccess_EXAMPLE_ROLE/${TMP_ROLE}/" "$new_file_path"
@@ -110,7 +102,7 @@ apply_substitutions_and_copy() {
     sed -i "s/EXAMPLE_DB_USER_PASSWORD/${DB_USER_PASSWORD}/" "$new_file_path"
     sed -i "s/MODERNIZATION_API_DB_USER/${MODERNIZATION_API_DB_USER}/" "$new_file_path"
     sed -i "s/MODERNIZATION_API_DB_USER_PASSWORD/${MODERNIZATION_API_DB_USER_PASSWORD}/" "$new_file_path"
-    sed -i "s/CERT_MANAGER_EMAIL/${CERT_MANAGER_EMAIL}/" "$new_file_path"
+    sed -i "s/EXAMPLE_EFS_ID/${EFS_ID}/" "$new_file_path"
     # Add more sed commands as needed for other placeholders
 }
 
@@ -179,6 +171,8 @@ apply_substitutions_and_copy "helm/charts/modernization-api/values.yaml" "helm/c
 apply_substitutions_and_copy "helm/charts/nbs-gateway/values.yaml" "helm/charts/nbs-gateway" "$SITE_NAME"
 apply_substitutions_and_copy "helm/charts/nginx-ingress/values.yaml" "helm/charts/nginx-ingress" "$SITE_NAME"
 apply_substitutions_and_copy "helm/charts/nifi-efs/values.yaml" "helm/charts/nifi-efs" "$SITE_NAME"
+apply_substitutions_and_copy "helm/charts/dataingestion-service/values.yaml" "helm/charts/dataingestion-service" "$SITE_NAME"
+apply_substitutions_and_copy "helm/charts/page-builder-api/values.yaml" "helm/charts/page-builder-api" "$SITE_NAME"
 
 echo "Configuration files have been updated and are ready for use."
 
