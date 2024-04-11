3637e2b1f38819f7ceecec4323886e8d077d6824
Chuck Moss <cmoss@enquizit.com>
2024-04-11 10:10:29 -0400

    added some logic to take args

--- scripts/install/aws_cloudshell_quickstart/10_cloudshell_install_helm_nginx-ingress.sh
+++ scripts/install/aws_cloudshell_quickstart/10_cloudshell_install_helm_nginx-ingress.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
 # must edit with each release or prompt and save
-HELM_VER=v1.0.0
+
+
+# Default file for storing selected values and entered credentials
+HELM_VER=v7.3.3
 echo "edit line 4 and comment out exit, then rerun"
 exit 1
 
 INGRESS_VER=4.7.2
 INSTALL_DIR=~/nbs_install
+
+DEFAULTS_FILE="nbs_defaults.sh"
+
+# Function to load saved defaults
+load_defaults() {
+    if [ -f "$DEFAULTS_FILE" ]; then
+        source "$DEFAULTS_FILE"
+    fi
+}
+
+update_defaults() {
+    local var_name=$1
+    local var_value=$2
+    if grep -q "^${var_name}_DEFAULT=" "$DEFAULTS_FILE"; then
+        sed -i "s/^${var_name}_DEFAULT=.*/${var_name}_DEFAULT=${var_value}/" "${DEFAULTS_FILE}"
+    else
+        echo "${var_name}_DEFAULT=${var_value}" >> "${DEFAULTS_FILE}"
+    fi
+}
+
+
+read -p "Please enter NBS Helm release version [${HELM_VER_DEFAULT}]: " HELM_VER  && HELM_VER=${HELM_VER:-$HELM_VER_DEFAULT}
+update_defaults "HELM_VER" "$HELM_VER"
+
 HELM_DIR=${INSTALL_DIR}/nbs-helm-${HELM_VER}
 cd ${HELM_DIR}/charts
 
