5b70fd3dc61547bca687df4c9af5a7a5e2493e57
szamfir <78826267+szamfir@users.noreply.github.com>
2024-04-16 14:22:11 -0400

    tidy

--- terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
@@ -9,5 +9,5 @@ resource "azurerm_private_dns_zone" "private_dns_zone_blob" {
 
 resource "azurerm_private_dns_zone" "private_dns_zone_file" {
   name                = "privatelink.file.core.windows.net"
-  resource_group_name = data.azurerm_resource_group.main.name 
+  resource_group_name = data.azurerm_resource_group.main.name
 }
