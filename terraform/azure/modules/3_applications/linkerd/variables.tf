07628cc3de2c44e98417cfa0a9f3c28f2dffae7d
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2024-04-26 13:34:00 -0400

    linkerd on azure (#120)
    * linkerd on azure
    
    * updated linkerd azure tf
    
    * cleaned up
    
    * removed provider
    
    * fixed based on review
    
    * added providers.tf to linkerd module

---
+++ terraform/azure/modules/3_applications/linkerd/variables.tf
@@ -0,0 +1,28 @@
+
+variable "aks_cluster_name" {
+type = string
+default = "dev-aks" 
+}
+
+variable "linkerd_repository" {
+    default = "https://helm.linkerd.io/stable"
+}
+variable "linkerd_chart" {
+    default = "linkerd-crds"
+}
+variable "linkerd_namespace_name" {
+    default = "linkerd"
+}
+variable "linkerd_controlplane_chart" {
+    default = "linkerd/linkerd-control-plane"
+}
+variable "linkerd_viz_chart" {
+    default = "linkerd-viz"
+}
+variable "linkerd_viz_namespace_name" {
+    default = "linkerd-viz"
+}
+
+variable "resource_group_name" {
+    type = string
+}
