a0f2605ebc34cad58132fa29eaf726cc59666214
Nouman Javed <njaved@enquizit.com>
2024-05-30 16:36:03 -0400

    Getting SQL Endpoint using Data

--- terraform/azure/modules/1_nbs6/aci/main.tf
+++ terraform/azure/modules/1_nbs6/aci/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_container_group" "aci" {
     }
 
     environment_variables = {
-      DATABASE_ENDPOINT = var.aci_sql_database_endpoint
+      DATABASE_ENDPOINT = data.azurerm_sql_managed_instance.sqlmi_endpoint.fqdn
       GITHUB_RELEASE_TAG = var.aci_github_release_tag
     }
 
