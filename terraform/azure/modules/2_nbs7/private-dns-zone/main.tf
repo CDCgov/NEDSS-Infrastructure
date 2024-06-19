c0df094459427f05be2547a3bffe1d7d2b40aa1a
szamfir <78826267+szamfir@users.noreply.github.com>
2024-06-19 15:52:33 -0400

    change vnet link name for consistency

--- terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
@@ -21,7 +21,7 @@ data "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_blob" {
   for_each = data.azurerm_virtual_network.vnet
-  name                  = "${each.value.name}-link"
+  name                  = "${each.value.name}-blob-link"
   resource_group_name   = data.azurerm_resource_group.main.name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_blob.name
   virtual_network_id    = each.value.id 
@@ -29,7 +29,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_blob" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_file" {  
   for_each = data.azurerm_virtual_network.vnet
-  name                  = "${each.value.name}-link"
+  name                  = "${each.value.name}-file-link"
   resource_group_name   = data.azurerm_resource_group.main.name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_file.name
   virtual_network_id    = each.value.id  
