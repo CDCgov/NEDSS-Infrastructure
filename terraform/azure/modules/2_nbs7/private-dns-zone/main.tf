712380dbfab72ba1d8dc7504b97b6757086a3e15
szamfir <78826267+szamfir@users.noreply.github.com>
2024-06-19 15:14:26 -0400

    update logic

--- terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
@@ -11,3 +11,25 @@ resource "azurerm_private_dns_zone" "private_dns_zone_file" {
   name                = "privatelink.file.core.windows.net"
   resource_group_name = data.azurerm_resource_group.main.name
 }
+
+#VNET Link
+data "azurerm_virtual_network" "vnet" {
+    name = var.virtual_network_name
+    resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_blob" {  
+  name                  = "${var.virtual_network_name}-blob"
+  resource_group_name   = data.azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_blob.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+ 
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_file" {  
+  name                  = "${var.virtual_network_name}-file"
+  resource_group_name   = data.azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_file.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  
+}
--- terraform/azure/modules/2_nbs7/private-dns-zone/variables.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/variables.tf
@@ -2,3 +2,8 @@ variable "resource_group_name" {
   type        = string
   description = "Resource group name for existing and to be deployed azure resources"  
 }
+
+variable "virtual_network_name" {
+  type = string
+  description = "Name of virtual network to be associated as a virtual network link for the private dns zone."
+}
