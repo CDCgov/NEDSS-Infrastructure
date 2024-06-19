6e7b743c60c0fb873302c8a704a021b0dc9f37c5
szamfir <78826267+szamfir@users.noreply.github.com>
2024-06-19 15:30:53 -0400

    allow for list of links

--- terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/main.tf
@@ -14,22 +14,24 @@ resource "azurerm_private_dns_zone" "private_dns_zone_file" {
 
 #VNET Link
 data "azurerm_virtual_network" "vnet" {
-    name = var.virtual_network_name
+    for_each = toset(var.virtual_network_name)
+    name = each.key
     resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_blob" {  
-  name                  = "${var.virtual_network_name}-blob"
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_blob" {
+  for_each = data.azurerm_virtual_network.vnet
+  name                  = "${each.value.outputs.vnet.name}-blob"
   resource_group_name   = data.azurerm_resource_group.main.name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_blob.name
-  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  virtual_network_id    = each.value.outputs.vnet.id
  
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link_file" {  
-  name                  = "${var.virtual_network_name}-file"
+  name                  = "${each.value.outputs.vnet.name}-file"
   resource_group_name   = data.azurerm_resource_group.main.name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone_file.name
-  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  virtual_network_id    = each.value.outputs.vnet.id
   
 }
--- terraform/azure/modules/2_nbs7/private-dns-zone/variables.tf
+++ terraform/azure/modules/2_nbs7/private-dns-zone/variables.tf
@@ -4,6 +4,7 @@ variable "resource_group_name" {
 }
 
 variable "virtual_network_name" {
-  type = string
-  description = "Name of virtual network to be associated as a virtual network link for the private dns zone."
+  type = list(string)
+  description = "StringList of virtual network names to be associated as a virtual network link for the private dns zone."
+  default = []
 }
