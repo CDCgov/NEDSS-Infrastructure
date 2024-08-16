5c2de080e10b537bad5287d349991aedeaad9b46
Chuck Moss <cmoss@enquizit.com>
2024-08-16 18:42:22 -0400

    working on conditional/ternary operator for user data

--- terraform/aws/development-infrastructure/nbs-legacy/main.tf
+++ terraform/aws/development-infrastructure/nbs-legacy/main.tf
@@ -43,7 +43,8 @@ module "app_server" {
   # cmoss - we can now disable user data
   # by passing enable_user_data = false
   # user_data = <<EOF
-  user_data = var.enable_user_data ? <<EOF
+  #user_data = var.enable_user_data ? <<EOF
+  user_data = var.enable_user_data ? <<EOF ... EOF : null
 <powershell>
 #Initialize hastable for data sources
 $connectionURLs = @{ "NedssDS" = "jdbc:sqlserver://${var.nbs_db_dns}:1433;SelectMethod=direct;DatabaseName=nbs_odse";
