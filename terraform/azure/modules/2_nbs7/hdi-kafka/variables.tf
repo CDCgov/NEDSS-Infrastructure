e48115afc9993f20a7930af4e074259c8fbee2b5
Ozair Tukhi <128056734+tukhi@users.noreply.github.com>
2024-08-21 13:37:41 -0400

    removed storage account var

--- terraform/azure/modules/2_nbs7/hdi-kafka/variables.tf
+++ terraform/azure/modules/2_nbs7/hdi-kafka/variables.tf
@@ -9,9 +9,9 @@ variable "location" {
   type        = string
 }
 
-variable "storage_account_name" {
-      type        = string
-}
+# variable "storage_account_name" {
+#       type        = string
+# }
 
 variable "account_tier" {
     type        = string
