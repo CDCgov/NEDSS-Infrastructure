89a62772c9d71906e6c1deb40663fdebb5bddec3
szamfir <szamfir@enquizit.com>
2024-10-02 14:24:55 -0400

    change root file system for nbs6

--- terraform/aws/development-infrastructure/nbs-legacy/nbs-classic-ecs-task.tf
+++ terraform/aws/development-infrastructure/nbs-legacy/nbs-classic-ecs-task.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "task" {
       name  = "${var.resource_prefix}-task",
       image = "${var.docker_image}",
       tags = var.tags,
-      readonlyRootFilesystem = true,
+      readonlyRootFilesystem = false,
       portMappings = [
         {
           containerPort = 7001,
