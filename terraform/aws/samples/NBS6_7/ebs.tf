a7a741b761297a0ccab08810ffb53ae2ab411f9a
Chuck Moss <cmoss@enquizit.com>
2025-01-15 16:30:18 -0500

    fix linefeed

--- terraform/aws/samples/NBS6_7/ebs.tf
+++ terraform/aws/samples/NBS6_7/ebs.tf
@@ -1,8 +1,7 @@
 # Serial: 2025011501
 
 module "ebs" {
-  source =
-"git::https://github.com/CDCgov/NEDSS-Infrastructure.git//terraform/aws/development-infrastructure/ebs?ref=v1.2.23"
+  source = "git::https://github.com/CDCgov/NEDSS-Infrastructure.git//terraform/aws/development-infrastructure/ebs?ref=v1.2.23"
   #source  = "../../../../NEDSS-Infrastructure/terraform/aws/development-infrastructure/ebs"
 
 }
