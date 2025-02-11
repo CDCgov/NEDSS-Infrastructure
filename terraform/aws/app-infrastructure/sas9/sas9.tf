4da91260bc95fa35aba65e35bd98f87cb2bf686f
Ozair Tukhi <usq9@cdc.com>
2025-02-10 19:27:15 -0500

    fixed some issues with sas module

--- terraform/aws/app-infrastructure/sas9/sas9.tf
+++ terraform/aws/app-infrastructure/sas9/sas9.tf
@@ -55,13 +55,20 @@ resource "aws_security_group" "sas_sg" {
 
 # EC2 Instance with IAM role and security group
 resource "aws_instance" "sas9" {
-  ami           = var.sas9_ami # "ami-0599348d3e7f1a34e"  # SAS ami in shared services account. sas9.4-02-07-2025
-  instance_type = "t2.medium"
-  key_name      = "SAS-keypair"          # Create this key in the account before launching
+  ami           = var.sas_ami # "ami-0599348d3e7f1a34e"  # SAS ami in shared services account. sas9.4-02-07-2025
+  instance_type = var.sas_instance_type  # "t2.medium"
+  key_name      = var.sas_keypair_name  # Create this key in the account before launching
 
   iam_instance_profile = aws_iam_role.sas_role.name
-  security_groups      = [aws_security_group.sas-sg.name]
+  security_groups      = [aws_security_group.sas_sg.name]
 
+  root_block_device {
+    device_name = "/dev/xvda"  # This is the root device for most Linux instances
+    volume_size = 200         # The size of the root volume, which is 200GB in your case
+    volume_type = "gp3"       # You can change this to another volume type (e.g., gp2, io1)
+    encrypted   = true        # Enable encryption for the root volume
+    kms_key_id  = aws_kms_key.my_kms_key.id  # Reference the KMS key for encryption
+  }
   tags = {
     Name = "SAS9.4"
   }
--- terraform/aws/app-infrastructure/sas9/variables.tf
+++ terraform/aws/app-infrastructure/sas9/variables.tf
@@ -7,6 +7,23 @@ variable "vpc_cidr_block" {
     description = "vpc cidr allowing traffic to rds and wildfly"
 }
 
-variable "sas9_ami" {
+variable "sas_ami" {
     description = "sas9 ami from shared services account"
+}
+
+variable "sas_keypair_name" {
+    description = "sas9 ami from shared services account"
+}
+
+variable "sas_instance_type" {
+    description = "sas9 ami from shared services account"
+}
+
+variable "sas_kms_key_id" {
+    description = "kms key arn to be used to encrypt root volume"
+}
+
+variable "sas_root_volume_size" {
+    description = "root volume size for sas server"
+    default = "200"
 }
