9e7f588dc2bd99658cbe0399a76706c0f2c965ae
Ozair Tukhi <usq9@cdc.com>
2025-02-10 21:15:33 -0500

    nnn

--- terraform/aws/app-infrastructure/sas9/sas9.tf
+++ terraform/aws/app-infrastructure/sas9/sas9.tf
@@ -57,6 +57,9 @@ resource "aws_security_group" "sas_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    tags = {
+    Name = "sas-sg"
+  }
 }
 
 # EC2 Instance with IAM role and security group
