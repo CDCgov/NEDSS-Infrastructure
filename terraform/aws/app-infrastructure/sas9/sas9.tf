7712da74a77e235ca373532ac3b8540e4495265f
Ozair Tukhi <usq9@cdc.com>
2025-02-13 09:10:22 -0500

    changed intra vpc security group rule to all traffic

--- terraform/aws/app-infrastructure/sas9/sas9.tf
+++ terraform/aws/app-infrastructure/sas9/sas9.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "sas_sg" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = [var.vpc_cidr_block]
   }
 
