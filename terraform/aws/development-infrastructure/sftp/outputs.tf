1145e651e9d20827f97fdce425faf3d2aa970736
Chuck Moss <cmoss@enquizit.com>
2025-03-27 17:34:33 -0400

    fixed error with conditional

--- terraform/aws/development-infrastructure/sftp/outputs.tf
+++ terraform/aws/development-infrastructure/sftp/outputs.tf
@@ -1,3 +1,5 @@
+
+
 output "bucket_name" {
   description = "The name of the S3 bucket used for HL7 file uploads"
   value       = aws_s3_bucket.hl7.id
@@ -11,18 +13,15 @@ output "dynamodb_table_name" {
 output "sns_error_topic_arn" {
   description = "ARN of the SNS topic for error notifications"
   value       = try(aws_sns_topic.error[0].arn, null)
-  condition   = var.enable_error_notifications
 }
 
 output "sns_success_topic_arn" {
   description = "ARN of the SNS topic for success notifications"
   value       = try(aws_sns_topic.success[0].arn, null)
-  condition   = var.enable_success_notifications
 }
 
 output "sns_summary_topic_arn" {
   description = "ARN of the SNS topic for summary notifications"
   value       = try(aws_sns_topic.summary[0].arn, null)
-  condition   = var.enable_summary_notifications
 }
 
