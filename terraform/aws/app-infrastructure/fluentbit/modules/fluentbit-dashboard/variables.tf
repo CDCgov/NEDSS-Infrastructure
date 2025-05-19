5e6936a1403f50108f1618e6dcbdc55923c2bfd2
Ozair Tukhi <usq9@cdc.com>
2025-05-19 15:53:42 -0400

    added RTRs to fluentbit - cloudwatch

--- terraform/aws/app-infrastructure/fluentbit/modules/fluentbit-dashboard/variables.tf
+++ terraform/aws/app-infrastructure/fluentbit/modules/fluentbit-dashboard/variables.tf
@@ -2,7 +2,7 @@ variable "log_group_name" { default = "fluent-bit-cloudwatch"}
 
 variable "app_name" {
   type    = list(string)
-  default = ["argocd", "elasticsearch", "fluentbit", "ingress-nginx", "istio", "nifi", "patient-search", "prometheus", "modernization-api", "dataingestion"]
+  default = ["argocd", "elasticsearch", "fluentbit", "ingress-nginx", "nifi", "prometheus", "modernization-api", "dataingestion-service", "data-processing-service", "debezium-service-connect", "investigation-reporting-service", "kafka-rtr-sink-connector-cp-kafka-connect", "keycloak-deployment", "ldfdata-reporting-service", "liquibase-service", "nnd-service", "observation-reporting-service", "organization-reporting-service", "page-builder-api", "person-reporting-service", "post-processing-reporting-service" ]
 }
 
 variable "resource_prefix" {}
