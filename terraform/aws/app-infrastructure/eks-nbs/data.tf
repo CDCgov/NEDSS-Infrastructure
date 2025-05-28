0255df35ef64a5dafd5f6f4a6adfa8622f859cfc
Serban Zamfir <szamfir@enquizit.com>
2025-05-28 14:27:15 -0400

    add data for current user

--- terraform/aws/app-infrastructure/eks-nbs/data.tf
+++ terraform/aws/app-infrastructure/eks-nbs/data.tf
@@ -13,3 +13,12 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+data "aws_iam_session_context" "current" {
+   # This data source provides information on the IAM source role of an STS assumed role
+  # For non-role ARNs, this data source simply passes the ARN through issuer ARN
+  # Ref https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2327#issuecomment-1355581682
+  # Ref https://github.com/hashicorp/terraform-provider-aws/issues/28381
+  arn = try(data.aws_caller_identity.current[0].arn, "")
+}
