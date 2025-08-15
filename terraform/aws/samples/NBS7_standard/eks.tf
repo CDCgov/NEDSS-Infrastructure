f9f031d876b58f2b4bd552d56b96de684cbd6557
Kashyap Ramakur <kramakur@enquizit.com>
2025-08-15 16:24:59 -0400

    updates for 7.11.0

--- terraform/aws/samples/NBS7_standard/eks.tf
+++ terraform/aws/samples/NBS7_standard/eks.tf
@@ -8,7 +8,8 @@ module "eks_nbs" {
   vpc_id                     = module.modernization-vpc.vpc_id
   aws_role_arn               = "arn:aws:iam::${var.target_account_id}:role/${var.aws_admin_role_name}"
   sso_role_arn               = "arn:aws:iam::${var.target_account_id}:role/${var.aws_admin_role_name}"
-  readonly_role_arn          = "arn:aws:iam::${var.target_account_id}:role/${var.readonly_role_name}"
+  # optional role to authenticate into the EKS cluster for ReadOnly, leave empty "" if not needed
+  readonly_role_arn          = ""
   #sso_role_arn               = "arn:aws:iam::${var.target_account_id}:role/${var.sso_admin_role_name}"
   desired_nodes_count        = var.eks_desired_nodes_count
   max_nodes_count            = var.eks_max_nodes_count
