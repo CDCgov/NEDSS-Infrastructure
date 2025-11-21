2ab52f1b01963d776e38d2e7cb3d5eeb4847b97d
Mike Ward <mwardwebdevelopment@gmail.com>
2025-11-21 10:45:18 -0500

    Update Helm provider configuration

--- terraform/aws/app-infrastructure/linkerd/providers.tf
+++ terraform/aws/app-infrastructure/linkerd/providers.tf
@@ -26,7 +26,7 @@ terraform {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = var.eks_cluster_endpoint                             # module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data) # base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
