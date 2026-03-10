19df2d4a077eb851123043a0b17c30844bf2bcd6
Mike Ward <mwardwebdevelopment@gmail.com>
2026-03-10 14:27:30 -0400

    feat(aws): layered terraform

--- terraform/aws/app-infrastructure/fluentbit/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.eks_cluster_name
-}
--- terraform/aws/app-infrastructure/fluentbit/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "fluentbit_role_arn" {
-  description = "fluentbit role arn"
-  value       = module.iam.fluentbit_role_arn
-}
-
-
-
