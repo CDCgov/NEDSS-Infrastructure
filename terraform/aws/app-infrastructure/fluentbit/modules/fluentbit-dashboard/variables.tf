c953b55ac8e927a39ddc626be474231e8da52618
Mike Ward <mwardwebdevelopment@gmail.com>
2026-03-10 14:27:30 -0400

    feat(aws): layered terraform

--- terraform/aws/app-infrastructure/fluentbit/modules/fluentbit-dashboard/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "log_group_name" { default = "fluent-bit-cloudwatch" }
-
-variable "app_name" {
-  type    = list(string)
-  default = ["argocd", "elasticsearch", "fluentbit", "ingress-nginx", "istio", "nifi", "patient-search", "prometheus", "modernization-api", "dataingestion"]
-}
-
-variable "resource_prefix" {}
--- terraform/aws/samples/3-applications/linkerd.tf
+++ terraform/aws/samples/3-applications/linkerd.tf
@@ -1,5 +1,5 @@
 module "linkerd" {
-  source = "../..//app-infrastructure/linkerd"
+  source = "../../modules/3-applications/linkerd"
 
   eks_cluster_endpoint               = local.eks_cluster_endpoint
   cluster_certificate_authority_data = local.cluster_certificate_authority_data
