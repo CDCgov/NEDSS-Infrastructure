7d13d57fd176634b55ca713dec16dbb6ffdec2bf
Serban Zamfir <serban.zamfir@skylight.digital>
2026-04-10 15:10:01 -0400

    add IRSA role for datacompare

--- terraform/aws/app-infrastructure/eks-nbs/irsa.tf
+++ terraform/aws/app-infrastructure/eks-nbs/irsa.tf
@@ -48,7 +48,9 @@ module "datacompare_irsa_role" {
       namespace_service_accounts = var.datacompare_namespace_and_service
     }
   }
-  policies = aws_iam_policy.datacompare_irsa_policy.arn
+  policies ={
+   policy = aws_iam_policy.datacompare_irsa_policy.arn
+  } 
 }
 
 resource "aws_iam_policy" "datacompare_irsa_policy" {
