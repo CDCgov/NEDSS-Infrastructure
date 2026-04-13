5e5933133de13669c247328348655bff34849a1e
Emmanuel Nwakire <100969358+nuel247@users.noreply.github.com>
2026-04-13 14:11:10 -0400

    add OTEL collector IRSA to eks-nbs

--- terraform/aws/app-infrastructure/otel-collector/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "otel_collector_role_arn" {
-  description = "OTEL Collector IAM role ARN (use with --set serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn=<ARN>)"
-  value       = module.iam.otel_collector_role_arn
-}
--- terraform/aws/app-infrastructure/otel-collector/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.21.0, < 7.0.0"
-    }
-  }
-  required_version = ">= 1.13.3"
-}
