053d80f34a149c0a7a39d7bac7d1cb727eb106f2
Mike Ward <mwardwebdevelopment@gmail.com>
2026-04-28 13:59:22 -0400

    feat(azure): public dns zone module

---
+++ terraform/azure/modules/0_landing_zone/public-dns-zone/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "public_domain_name" {
+  type        = string
+  description = "The root domain (e.g., example.com)"
+}
