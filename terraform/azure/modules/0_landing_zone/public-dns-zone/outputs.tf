ea230568065cc5ec0f9487a0a0d4ace99ce16369
Mike Ward <mwardwebdevelopment@gmail.com>
2026-05-15 09:42:31 -0400

    Add enabled flag

--- terraform/azure/modules/0_landing_zone/public-dns-zone/outputs.tf
+++ terraform/azure/modules/0_landing_zone/public-dns-zone/outputs.tf
@@ -1,4 +1,4 @@
 output "dns_zone_id" {
-  value = azurerm_dns_zone.public.id
+  value = try(azurerm_dns_zone.public[0].id, "Module not enabled")
 }
 
--- terraform/azure/modules/0_landing_zone/public-dns-zone/variables.tf
+++ terraform/azure/modules/0_landing_zone/public-dns-zone/variables.tf
@@ -1,9 +1,31 @@
+variable "enabled" {
+  type = bool
+  description = "Enable the module"
+  default = true
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Resource group name"
+  default = ""
+
+  validation {
+    condition = var.enabled && var.resource_group_name != ""
+
+    error_message = "resource_group_name must have a value if module is enabled"
+  }
 }
 
 variable "public_domain_name" {
   type        = string
   description = "The root domain (e.g., example.com)"
+  default = ""
+
+    validation {
+    condition = var.enabled && var.public_domain_name != ""
+
+    error_message = "public_domain_name must have a value if module is enabled"
+  }
+}
+
 }
