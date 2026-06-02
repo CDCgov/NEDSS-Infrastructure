e0a556fd41f94e5a951a56c0d58b813c5b492547
Mike Ward <mwardwebdevelopment@gmail.com>
2026-06-02 10:41:09 -0400

    Rename files

--- terraform/azure/samples/0-landing-zone/resource-group.tf
+++ terraform/azure/samples/0-landing-zone/resource-group.tf
@@ -1,7 +1,7 @@
 module "rg" {
   source = "../../0_landing_zone/resource_group"
-  
-  enabled = var.rg_enabled
+
+  enabled             = var.rg_enabled
   resource_group_name = var.resource_group_name
-  location = var.location
+  location            = var.location
 }
