80a888acd69f3998b269c49a1ffe33e3584ce1bb
Mike Ward <mwardwebdevelopment@gmail.com>
2026-06-03 09:38:58 -0400

    Remove extra slash

--- terraform/azure/samples/0-landing-zone/public-dns-zone.tf
+++ terraform/azure/samples/0-landing-zone/public-dns-zone.tf
@@ -1,5 +1,5 @@
 module "public_dns" {
-  source = "../..//0_landing_zone/public-dns-zone"
+  source = "../../0_landing_zone/public-dns-zone"
 
   public_domain_name  = var.public_domain_name
   resource_group_name = var.resource_group_name
