5c7a73a45ba68cb1a349e34459dbf2a50140c24a
Mike Ward <mwardwebdevelopment@gmail.com>
2026-06-03 14:36:34 -0400

    Remove resource group file, add enabled to public dns zone

--- terraform/azure/samples/0-landing-zone/public-dns-zone.tf
+++ terraform/azure/samples/0-landing-zone/public-dns-zone.tf
@@ -1,6 +1,7 @@
 module "public_dns" {
   source = "../../0_landing_zone/public-dns-zone"
 
+  enabled             = var.enabled
   public_domain_name  = var.public_domain_name
   resource_group_name = var.resource_group_name
 }
--- terraform/azure/samples/0-landing-zone/resource-group.tf
+++
@@ -1,7 +0,0 @@
-module "rg" {
-  source = "../../0_landing_zone/resource_group"
-
-  enabled             = var.rg_enabled
-  resource_group_name = var.resource_group_name
-  location            = var.location
-}
