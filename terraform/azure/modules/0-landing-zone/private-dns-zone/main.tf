ec5d73a1ded8ae3ea17c5f5e43be8213560b3b84
Mike Ward <mwardwebdevelopment@gmail.com>
2026-06-10 09:10:05 -0400

    feat(azure): Add enabled flag to private dns zone

--- terraform/azure/modules/0-landing-zone/private-dns-zone/main.tf
+++ terraform/azure/modules/0-landing-zone/private-dns-zone/main.tf
@@ -1,12 +1,14 @@
 resource "azurerm_private_dns_zone" "private" {
+  count               = var.enabled ? 1 : 0
   name                = var.private_dns_zone_name
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "private" {
-  name                  = "${var.vnet_name}-dns-link"
+  count                 = var.enabled ? 1 : 0
+  name                  = "${var.vnet_name}-private-dns-link"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.private.name
+  private_dns_zone_name = azurerm_private_dns_zone.private[count.index].name
   virtual_network_id    = var.vnet_id
   registration_enabled  = true
 }
