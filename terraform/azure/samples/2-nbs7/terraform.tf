80a001d342980671f9f04354a5057ba8607679c0
Josh Olson <josh.olson@skylight.digital>
2026-07-06 16:05:22 -0500

    Azure Layers 2 and 3: Add commentary that references layer 0

--- terraform/azure/samples/2-nbs7/terraform.tf
+++ terraform/azure/samples/2-nbs7/terraform.tf
@@ -1,3 +1,5 @@
+# NOTE: Please see the commentary in ../0-landing-zone/terraform.tf which is also applicable to this file.
+
 terraform {
   required_version = ">= 1.13.3"
 
@@ -6,6 +8,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=4.68, <5.0"
     }
+  }
+
+  backend "azurerm" {
+    use_azuread_auth = true # Use Microsoft Entra ID authentication
+
+    tenant_id = "EXAMPLE_EXISTING_TENANT_ID"
+
+    storage_account_name = "EXAMPLE_EXISTING_STORAGE_ACCOUNT_NAME"
+    container_name       = "EXAMPLE_EXISTING_STORAGE_ACCOUNT_CONTAINER_NAME"
+    key                  = "EXAMPLE_BLOB_NAME_TO_CREATE"
   }
 }
 
--- terraform/azure/samples/3-applications/terraform.tf
+++ terraform/azure/samples/3-applications/terraform.tf
@@ -1,6 +1,4 @@
-locals {
-  admin_config = yamldecode(data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw)
-}
+# NOTE: Please see the commentary in ../0-landing-zone/terraform.tf which is also applicable to this file.
 
 terraform {
   required_version = ">= 1.13.3"
@@ -13,20 +11,24 @@ terraform {
   }
 
   backend "azurerm" {
-    use_azuread_auth     = true
-    tenant_id            = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = "/0-landing-zone.tfstate"
-  }
-}
+    use_azuread_auth = true # Use Microsoft Entra ID authentication
 
-locals {
-  environment = "dev1"
+    tenant_id = "EXAMPLE_EXISTING_TENANT_ID"
+
+    storage_account_name = "EXAMPLE_EXISTING_STORAGE_ACCOUNT_NAME"
+    container_name       = "EXAMPLE_EXISTING_STORAGE_ACCOUNT_CONTAINER_NAME"
+    key                  = "EXAMPLE_BLOB_NAME_TO_CREATE"
+  }
 }
 
 provider "azurerm" {
   features {}
+}
+
+locals {
+  environment = "dev1"
+
+  admin_config = yamldecode(data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw)
 }
 
 provider "helm" {
