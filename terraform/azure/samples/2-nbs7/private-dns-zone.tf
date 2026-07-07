5b52b9b571b34300b40499ac2ef210fe521d7965
Josh Olson <josh.olson@skylight.digital>
2026-07-07 09:16:20 -0500

    Azure Terraform samples: Delete this private-dns-zone.tf file from layer 2, because it exists in layer 0

--- terraform/azure/samples/2-nbs7/private-dns-zone.tf
+++
@@ -1,6 +0,0 @@
-module "private_dns_zone" {
-  source = "../../modules/2-nbs7/private-dns-zone"
-
-  resource_group_name  = var.vnet_resource_group_name
-  virtual_network_name = var.private_dns_virtual_network_name
-}
