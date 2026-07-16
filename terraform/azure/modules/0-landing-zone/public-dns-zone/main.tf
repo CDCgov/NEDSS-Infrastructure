28361c30fb56e72a9bfda1c4eb307d91251216c1
Mike Ward <mike.ward@skylight.digital>
2026-07-16 09:58:03 -0400

    fix(azure): make registration_enabled optional (#322)

--- terraform/azure/modules/0-landing-zone/public-dns-zone/main.tf
+++ terraform/azure/modules/0-landing-zone/public-dns-zone/main.tf
@@ -9,7 +9,7 @@ module "dns_records" {
   for_each = var.dns_records
 
   resource_group_name = var.resource_group_name
-  zone_name           = azurerm_private_dns_zone.private.name
+  zone_name           = azurerm_private_dns_zone.private[0].name
 
   record_name  = each.value.record_name
   record_type  = each.value.record_type
