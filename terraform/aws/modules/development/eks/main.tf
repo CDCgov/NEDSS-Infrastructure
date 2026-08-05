f063ed6270007abc1a76190e27cbfcbef5654fcc
Mike Ward <mwardwebdevelopment@gmail.com>
2026-08-05 14:39:24 -0400

    tflint: terraform_deprecated_index

--- terraform/aws/modules/development/eks/main.tf
+++ terraform/aws/modules/development/eks/main.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes = {
     host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--role-arn", var.aws_role_arn]
@@ -12,7 +12,7 @@ provider "helm" {
 
 provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--role-arn", var.aws_role_arn]
