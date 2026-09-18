8ed7643917de64fd86784275da84ab6f1e4561d5
Serban Zamfir <serban.zamfir@skylight.digital>
2026-09-18 12:36:37 -0400

    fix from 30 hours to 30 days

--- terraform/aws/modules/2-applications/linkerd/variables.tf
+++ terraform/aws/modules/2-applications/linkerd/variables.tf
@@ -25,7 +25,7 @@ variable "linkerd_helm_version" {
 variable "early_renewal_hours" {
   description = "The resource will consider the certificate to have expired the given number of hours before its actual expiry time."
   type        = number
-  default     = 30
+  default     = 720
 }
 
 ############ linkerd crds variable: ############
